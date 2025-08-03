4e25f3d02ca32282eebb9d490585b8ebd3528006
25tal <72045932+51talcol21@users.noreply.github.com>
2025-08-02 20:46:58 -0700

    Added filters/filename fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,26 +8,127 @@ type ImageEntry = {
   date: string;
   comments: string;
   tags: string[];
+  location: string;
+  speckled: string;
+  alive: string,
 };
+
+type Filter = {
+  tags: string[];
+  locations: string[];
+  alive: string;
+  speckled: string[];
+}
 
 export default function GalleryPage() {
   const [images, setImages] = useState<ImageEntry[]>([]);
+  const [availableLocations, setAvailableLocations] = useState<string[]>([])
+  const [availableSpeckles, setSpeckles] = useState<string[]>([])
+  const [filter, setFilter] = useState<Filter>({
+    tags: [],
+    locations: [],
+    alive: 'Alive',
+    speckled: [],
+  })
   const { basePath } = useRouter();
 
   useEffect(() => {
     fetch(`${basePath}/images.json`)
       .then(res => res.json())
-      .then(setImages);
+      .then((data: ImageEntry[]) => {
+        setImages(data)
+
+        const locationsSet = new Set(data.map(img => img.location))
+          setAvailableLocations(Array.from(locationsSet))
+
+        const availableSpeckles = new Set(data.map(img => img.speckled))
+          setSpeckles(Array.from(availableSpeckles))
+        })
   }, []);
 
+  const handleLocationChange = (location: string) => {
+    if (location === "") {
+      setFilter({...filter, locations: []})
+      return
+    }
+
+    setFilter(prev => {
+      const isSelected = prev.locations.includes(location)
+      return {
+        ...prev,
+        locations: isSelected
+          ? prev.locations.filter(loc => loc !== location)
+          : [...prev.locations, location],
+      }
+    })
+  }
+
+  const handleSpeckleChange = (location: string) => {
+    setFilter(prev => ({ ...prev, speckled: [location] }))
+  }
+
+  const filteredImages = images.filter(images => {
+    const matchesTags =
+      filter.tags.length === 0 || filter.tags.every(tag => images.tags.includes(tag))
+  
+      console.log(filter.locations, images.location)
+    const matchesLocations =
+      filter.locations.length === 0 || filter.locations.includes(images.location)
+
+    const matchesSpeckles =
+      filter.speckled.length === 0 || filter.speckled.includes(images.speckled)
+  
+    return matchesTags && matchesLocations && matchesSpeckles
+  })
+
   return (
-    <div className="p-4 flex flex-wrap gap-4">
-      {images.map((img) => (
-        <div key={img.filename} className="w-48">
-          <img src={`${basePath}/images/${img.filename}`} alt={img.filename} />
-          <p className="text-sm">{img.date} {img.time}</p>
+    <>
+      <div className="flex flex-row">
+        <div>
+          <label className="block mb-2 font-medium">Filter by location</label>
+          <div className="flex flex-col">
+          {availableLocations.map(tag => {
+              const isSelected = filter.locations.includes(tag);
+               return (
+                <button onClick={() => handleLocationChange(tag)} key={tag} value={tag} className={`cursor-pointer px-3 py-1 rounded border ${isSelected ? 'bg-blue-500 text-white border-blue-700' : 'bg-white text-gray-700 border-gray-300'}
+                focus:outline-none`}>
+                  {tag}
+                </button>
+          )})}
+          </div>
+        </div>
+        <div>
+          <label className="block mb-2 font-medium">Filter by location</label>
+          <select
+            multiple
+            onChange={(e) => handleSpeckleChange(e.target.value)}
+            className="block w-min h-40 p-2 border rounded mb-6"
+          >
+            {availableSpeckles.map(tag => (
+              <option key={tag} value={tag}>
+                {tag}
+              </option>
+            ))}
+          </select>
+          {availableLocations.map(tag => {
+              const isSelected = filter.locations.includes(tag);
+               return (
+                <button onClick={() => handleLocationChange(tag)} key={tag} value={tag} className={`cursor-pointer px-3 py-1 rounded border ${isSelected ? 'bg-blue-500 text-white border-blue-700' : 'bg-white text-gray-700 border-gray-300'}
+                focus:outline-none`}>
+                  {tag}
+                </button>
+          )})}
+        </div>
+      </div>
+
+      <div className="p-4 flex flex-wrap items-stretch gap-4">
+      {filteredImages.map((img) => (
+        <div key={img.filename} className="flex flex-col w-48 border-darkpink border-2 p-2 bg-light-light-pink">
+            <img src={`${basePath}/images/${img.filename}`} alt={img.filename} className="h-auto w-full"/>
+            <div className="text-sm mt-auto">{img.date} {img.time} <br /> Location: {img.location} <br /> Speckled: {img.speckled} <br /> Status: {img.alive}</div>
         </div>
       ))}
     </div>
+    </>
   );
 }
--- src/scripts/generateImageData.js
+++ src/scripts/generateImageData.js
@@ -7,6 +7,27 @@ const outputFile = path.join(__dirname, '..', '..', 'public', 'images.json');
 const files = fs.readdirSync(imagesDir);
 const result = [];
 
+// File Name Format: monthday_time2400_location_isDead_speckled
+
+const directions = {
+  "SW": "Southwest",
+  "AP": "Apiary",
+  "CM": "Compost",
+  "FD": "Field"
+}
+
+const isDead = {
+  "D" : "Dead",
+  "A" : "Alive"
+}
+
+const speckled = {
+  "NO": "None",
+  "SO": "Some",
+  "FE": "Few",
+  "MA": "Many"
+}
+
 files.forEach((file) => {
   if (path.extname(file).toLowerCase() === '.png') {
     const name = path.basename(file, '.png');
@@ -16,13 +37,18 @@ files.forEach((file) => {
         date.splice(4,0," ");
         date[0] = date[0].toUpperCase();
         date.push(", 2025")
+
         let time = parts[1].split("");
         time.splice(2,0,":");
+
         result.push({
             filename: file,
             time: time.join(""),
             date: date.join(""),
             comments: "",
+            location: directions[parts[2]],
+            alive: isDead[parts[3]],
+            speckled: speckled[parts[4]],
             tags: []
         });
     }
